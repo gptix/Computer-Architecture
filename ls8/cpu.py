274b352cfbf7882526457438ce4b5d149d93559f
Jud Taylor <jud.taylor@protonmail.com>
2020-07-21 18:46:44 -0400

    Added MUL, load(), calc operand count

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,21 +1,29 @@
 import sys
 
 # commands
-HLT   =  0x01    # op-code x01
+HLT =  0x01    # op-code x01
 
-LDI    =  0x82  # Set the value of a register to an integer.
+LDI =  0x82  # Set the value of a register to an integer.
 
                 # Three bytes
                     # byte 0 - instruction
                     # byte 1 - register
                     # byte 2 - "immediate" a number 0 - 255
 
-
-PRN    =  0x47  # Prints a value.
+PRN =  0x47  # Prints a value.
                 # Two bytes
                     # byte 0 - instruction
                     # byte 1 - value to print
 
+MUL = 0xa2   # Multiplies contents of register A by register B, 
+             # places result in registerA
+                # Three bytes
+                    # byte 0 - instruction
+                    # byte 1 - registerA number
+                    # byte 2 - registerB number 
+
+def number_of_operands (command):
+    return (command & 0b11000000) >> 6
 
 class CPU:
     """Main CPU class."""
@@ -23,7 +31,7 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
 
-        self.ram = [0x00]*256
+        self.ram = [0]*256
         
         # 8 general-purpose 8-bit numeric registers R0-R7.
         # R5 is reserved as the interrupt mask (IM)
@@ -113,18 +121,42 @@ class CPU:
 
         print()
 
+    def load(self, file_name):
+        try:
+            address = 0
+            with open(file_name) as f:
+                for line in f:
+                    # split on any first '#' char
+                    line_data = line.split('#')[0]
+                    # strip any remaining whitespace
+                    line_data = line_data.strip()
+                    # ignore empty lines
+                    if line_data == '':
+                        continue
+                    # convert from string, interpret as binary
+                    line_data = int(line_data,2)
+                    self.ram_write(address, line_data)
+                    address +=1
+
+
+        except FileNotFoundError:
+            print(f'The specified file, {file_name}, does not exist.')
+
+
     def run(self):
         """Run the CPU."""
         running = True
     
         while running:
-            self.trace()
+            # self.trace()
             command = self.ram[self.PC]
             # print(f'Command is x{command:X}')
             if command == HLT:
                 # print(f'Command is HLT')
                 # input()
+                # self.PC += number_of_operands(command)
                 sys.exit("Hit an HLT command")
+
             elif command == LDI:
                 # print(f'Command is LDI')
                 reg = self.ram[self.PC+1]
@@ -132,18 +164,60 @@ class CPU:
                 self.registers[reg] = self.ram[self.PC+2] 
                 # print(f'Value to place is {self.ram[self.PC+2]}')  
                 # print(f'Register after placement is  {self.ram[self.PC+2]}')  
-                self.PC += 1
+                self.PC += number_of_operands(command)
                 # print(f'self.pc is now {self.PC}')  
+
             elif command == PRN:
                 # print(f'Command is PRN')
-                # print(self.ram[self.PC+1])
-                # print(f'Number to print is {self.ram[self.PC+1]}')
-                self.PC += 1
-                # print(f'self.pc is now {self.PC}')  
+                reg = self.ram[self.PC+1]
+                # print(f'Register to use is {reg}')
+                val = self.registers[reg]
+                # print(f'val is {val}')
+                print(f'{val}')
+                self.PC += number_of_operands(command)
+                # print(f'self.pc is now {self.PC}') 
+
+            elif command == MUL:
+                # print(f'Command is MUL')
+                registerA = self.ram[self.PC+1]
+                # print(f'register number for A is {registerA}')
+                registerB = self.ram[self.PC+2]
+                # print(f'register number for B is {registerB}')
+                a = self.registers[registerA]
+                # print(f'content of register A is {a}')
+                b = self.registers[registerB]
+                # print(f'content of register B is {b}')
+                result = (a * b) % 256
+                # print(f'result before mod is {(a * b):b}')
+                # print(f'result after mod is {(result):b}')
+                self.registers[registerA] = result
+                # print(f'Content of register A after setting is {self.registers[registerA]:b}')
+                
+                self.PC += number_of_operands(command)
+
             self.PC += 1
             # print(f'self.pc is now {self.PC}')
             # input()
 
+
 spanky = CPU()
-spanky.load()
+
+if len(sys.argv)<2:
+    print('Please enter the name of a file containing a program to load, like "$ ls8.py my_program.ls8"')
+    sys.exit()
+
+
+file_name = sys.argv[1]
+
+spanky.load(file_name)
+
+def dump_ram(length=16):
+    ctr = 0
+    for ctr in range(length):
+        print(f'ram row {ctr}: 0x{spanky.ram[ctr]:x}')
+        ctr += 1
+# 
+# dump_ram()
+
 spanky.run()
+
