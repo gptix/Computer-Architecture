1e5bf35639fbe49034e69d4d9c369673708f28b6
Jud Taylor <jud.taylor@protonmail.com>
2020-07-21 20:26:22 -0400

    Implemented branchtable.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,26 +1,6 @@
 import sys
 
-# commands
-HLT =  0x01    # op-code x01
 
-LDI =  0x82  # Set the value of a register to an integer.
-
-                # Three bytes
-                    # byte 0 - instruction
-                    # byte 1 - register
-                    # byte 2 - "immediate" a number 0 - 255
-
-PRN =  0x47  # Prints a value.
-                # Two bytes
-                    # byte 0 - instruction
-                    # byte 1 - value to print
-
-MUL = 0xa2   # Multiplies contents of register A by register B, 
-             # places result in registerA
-                # Three bytes
-                    # byte 0 - instruction
-                    # byte 1 - registerA number
-                    # byte 2 - registerB number 
 
 def number_of_operands (command):
     return (command & 0b11000000) >> 6
@@ -52,7 +32,54 @@ class CPU:
         # G Greater-than: during a CMP, set to 1 if registerA is greater than registerB, zero otherwise.
         # E Equal: during a CMP, set to 1 if registerA is equal to registerB, zero otherwise.
 
+        # Define operations
+        HLT =  0x01    # op-code x01
 
+        LDI =  0x82  # Set the value of a register to an integer.
+
+                        # Three bytes
+                            # byte 0 - instruction
+                            # byte 1 - register
+                            # byte 2 - "immediate" a number 0 - 255
+
+        PRN =  0x47  # Prints a value.
+                        # Two bytes
+                            # byte 0 - instruction
+                            # byte 1 - value to print
+
+        MUL = 0xa2   # Multiplies contents of register A by register B, 
+                    # places result in registerA
+                        # Three bytes
+                            # byte 0 - instruction
+                            # byte 1 - registerA number
+                            # byte 2 - registerB number 
+
+        # Set up the branch table
+        self.branchtable = {}
+        self.branchtable[HLT] = self.HLT
+        self.branchtable[LDI] = self.LDI
+        self.branchtable[PRN] = self.PRN
+        self.branchtable[MUL] = self.MUL
+
+    def HLT(self):
+        sys.exit("Hit an HLT command")
+
+    def LDI(self):
+        reg = self.ram[self.PC+1] # register in which to place val
+        self.registers[reg] = self.ram[self.PC+2]  # place the val
+
+    def PRN(self):
+        reg = self.ram[self.PC+1] # decide register to examine
+        val = self.registers[reg] # get the value
+        print(f'{val}')
+
+    def MUL(self):
+        registerA = self.ram[self.PC+1] # register to find multiplicand 1
+        registerB = self.ram[self.PC+2] # register to find multiplicand 2
+        a = self.registers[registerA] # get multiplicand 1
+        b = self.registers[registerB] # get multiplicand 2
+        result = (a * b) % 256 # multiply and mod 256 to fit 8 bits
+        self.registers[registerA] = result # place the result
 
     def ram_read(self, MAR):
         """Accept the address to read and return the value stored there.
@@ -60,15 +87,11 @@ class CPU:
         """
         return self.ram[MAR]
 
-
     def ram_write(self, MDR, val_to_write):
         """Should accept a value to write, and the address to write it to.
         MDR contains the data that was read or the data to write"""
         self.ram[MDR] = val_to_write
         return True
-
-    
-
 
     def load(self):
         """Load a program into memory."""
@@ -77,19 +100,19 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -150,54 +173,10 @@ class CPU:
         while running:
             # self.trace()
             command = self.ram[self.PC]
-            # print(f'Command is x{command:X}')
-            if command == HLT:
-                # print(f'Command is HLT')
-                # input()
-                # self.PC += number_of_operands(command)
-                sys.exit("Hit an HLT command")
-
-            elif command == LDI:
-                # print(f'Command is LDI')
-                reg = self.ram[self.PC+1]
-                # print(f'Register to use is {reg}')
-                self.registers[reg] = self.ram[self.PC+2] 
-                # print(f'Value to place is {self.ram[self.PC+2]}')  
-                # print(f'Register after placement is  {self.ram[self.PC+2]}')  
-                self.PC += number_of_operands(command)
-                # print(f'self.pc is now {self.PC}')  
-
-            elif command == PRN:
-                # print(f'Command is PRN')
-                reg = self.ram[self.PC+1]
-                # print(f'Register to use is {reg}')
-                val = self.registers[reg]
-                # print(f'val is {val}')
-                print(f'{val}')
-                self.PC += number_of_operands(command)
-                # print(f'self.pc is now {self.PC}') 
-
-            elif command == MUL:
-                # print(f'Command is MUL')
-                registerA = self.ram[self.PC+1]
-                # print(f'register number for A is {registerA}')
-                registerB = self.ram[self.PC+2]
-                # print(f'register number for B is {registerB}')
-                a = self.registers[registerA]
-                # print(f'content of register A is {a}')
-                b = self.registers[registerB]
-                # print(f'content of register B is {b}')
-                result = (a * b) % 256
-                # print(f'result before mod is {(a * b):b}')
-                # print(f'result after mod is {(result):b}')
-                self.registers[registerA] = result
-                # print(f'Content of register A after setting is {self.registers[registerA]:b}')
-                
-                self.PC += number_of_operands(command)
-
-            self.PC += 1
-            # print(f'self.pc is now {self.PC}')
-            # input()
+            func = self.branchtable[command]
+            func()
+            self.PC += number_of_operands(command) # per exact command            
+            self.PC += 1 # per cycle
 
 
 spanky = CPU()
@@ -205,7 +184,6 @@ spanky = CPU()
 if len(sys.argv)<2:
     print('Please enter the name of a file containing a program to load, like "$ ls8.py my_program.ls8"')
     sys.exit()
-
 
 file_name = sys.argv[1]
 
@@ -216,8 +194,7 @@ def dump_ram(length=16):
     for ctr in range(length):
         print(f'ram row {ctr}: 0x{spanky.ram[ctr]:x}')
         ctr += 1
-# 
+
 # dump_ram()
 
 spanky.run()
-
